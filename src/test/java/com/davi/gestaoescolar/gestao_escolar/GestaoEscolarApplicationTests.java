8c4cb1ad1530ce17ea72de765063587e1cc92d75
Davi Rosa Gomes <davi.rosa@gec.inatel.br>
2025-10-17 16:13:45 -0300

    Agora vai

--- src/test/java/com/davi/gestaoescolar/gestao_escolar/GestaoEscolarApplicationTests.java
+++ src/test/java/com/davi/gestaoescolar/gestao_escolar/GestaoEscolarApplicationTests.java
@@ -1,13 +1,9 @@
 package com.davi.gestaoescolar.gestao_escolar;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GestaoEscolarApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
